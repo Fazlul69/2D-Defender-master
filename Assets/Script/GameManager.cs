1f9dcd293901f69d3c3b5673c5c655f8a7f5c170
Fazlul69 <sawravm299@gmail.com>
2019-12-02 14:21:03 +0600

    update

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject spawnObj;
+    public int enemyInScene;
+    public int spawnRate;
+    public float spawnDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnRate = 8;
+        spawnDelay = 0.7f;
+      
+        StartCoroutine(spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public IEnumerator spawn() {
+        if (enemyInScene < 8) {
+
+            for(int i =0; i<spawnRate; i++)
+            {
+                GameObject newEnemy = Instantiate(enemies[0]);
+                newEnemy.transform.position = spawnObj.transform.position;
+                enemyInScene += 1;
+                yield return new WaitForSeconds(spawnDelay);
+            }
+            StartCoroutine(spawn());
+        }
+        
+    }
+}
---
+++ Assets/Script/roadScripts.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roadScripts : MonoBehaviour
+{
+    public Rigidbody playerRigid;
+    public GameObject roadCreate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float H = Input.GetAxis("Horizontal");
+        playerRigid.velocity = new Vector3(0,0,-2);
+    }
+
+    public void OnTriggerEnter(Collision col) {
+        if (col.gameObject.tag == "CheckPoint") {
+
+           GameObject newRoadpox = Instantiate(roadCreate);
+            newRoadpox.transform.position = roadCreate.po
+        }
+    }
+}
